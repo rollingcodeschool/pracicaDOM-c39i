2a08d1f87e20f41d36e35c5f2de9f9603fba73e1
Emilse Arias <emilsearias@gmail.com>
2023-03-29 10:44:03 -0300

    agregamos el borrar nodos hijos

--- js/app.js
+++ js/app.js
@@ -1,37 +1,56 @@
-const colores = ['primary', 'secondary', 'info', 'success', 'danger','warning'];
-let btnVerMas =  document.getElementById('btnVerMas');
+const colores = [
+  "primary",
+  "secondary",
+  "info",
+  "success",
+  "danger",
+  "warning",
+];
+let btnVerMas = document.getElementById("btnVerMas");
 // agrega un manejador de eventos
-btnVerMas.addEventListener('click', verMas);
+btnVerMas.addEventListener("click", verMas);
 
-
-function cambiarTitulo(){
-    console.log('aqui quiero cambiar el titulo');
-    //traer un elemento del html
-    let tituloPrincipal = document.querySelector('h1');
-    // let tituloPrincipal = document.getElementById('nombreId');
-    // let tituloPrincipal = document.getElementsByClassName('container');
-    // let tituloPrincipal = document.getElementsByTagName('h1');
-    console.log(tituloPrincipal.innerText);
-    tituloPrincipal.innerHTML = '<b>Titulo</b> modificado';
-    let colorAleatorio = getRandomColor(0, colores.length-1);
-    tituloPrincipal.className = `display-4 text-${colores[colorAleatorio]}`;
-
+function cambiarTitulo() {
+  console.log("aqui quiero cambiar el titulo");
+  //traer un elemento del html
+  let tituloPrincipal = document.querySelector("h1");
+  // let tituloPrincipal = document.getElementById('nombreId');
+  // let tituloPrincipal = document.getElementsByClassName('container');
+  // let tituloPrincipal = document.getElementsByTagName('h1');
+  console.log(tituloPrincipal.innerText);
+  tituloPrincipal.innerHTML = "<b>Titulo</b> modificado";
+  let colorAleatorio = getRandomColor(0, colores.length - 1);
+  tituloPrincipal.className = `display-4 text-${colores[colorAleatorio]}`;
 }
 
 function getRandomColor(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-function verMas(){
-    console.log('desde la funcion ver mas');
-    //buscar el nodo padre
-    const seccionNoticia = document.getElementById('seccionNoticia');
+function verMas() {
+  console.log("desde la funcion ver mas");
+  //buscar el nodo padre
+  const seccionNoticia = document.getElementById("seccionNoticia");
+  
+  if (btnVerMas.innerHTML === "Ver mas") {
     //crear un nuevo elemento
-    let parrafoNuevo = document.createElement('p');
-    parrafoNuevo.innerHTML = `<b>Lorem ipsum dolor</b> sit amet consectetur adipisicing elit. Aliquid tenetur aperiam fugiat natus quaerat consequatur laudantium temporibus reiciendis odio? Quo possimus neque minus reprehenderit inventore, repudiandae sequi sit delectus quis?`
-    parrafoNuevo.className = 'lead';
+    let parrafoNuevo = document.createElement("p");
+    parrafoNuevo.innerHTML = `<b>Lorem ipsum dolor</b> sit amet consectetur adipisicing elit. Aliquid tenetur aperiam fugiat natus quaerat consequatur laudantium temporibus reiciendis odio? Quo possimus neque minus reprehenderit inventore, repudiandae sequi sit delectus quis?`;
+    parrafoNuevo.className = "lead";
     //insertar el nodo en el DOM
     // seccionNoticia.appendChild(parrafoNuevo);// agrega al final
     // seccionNoticia.prepend(parrafoNuevo); //agrega al principio
     seccionNoticia.insertBefore(parrafoNuevo, btnVerMas);
+
+    //modificar el boton
+    btnVerMas.innerHTML = "Ocultar";
+    btnVerMas.className = "btn btn-danger";
+  }else{
+    //eliminar un nodo del dom
+    console.log(seccionNoticia.children[3]);
+    seccionNoticia.removeChild(seccionNoticia.children[3]);
+    //resetear el boton
+    btnVerMas.innerHTML = 'Ver mas';
+    btnVerMas.className = 'btn btn-primary';
+  }
 }
